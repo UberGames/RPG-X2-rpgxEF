970cd5d2aa25759a8081c28ab6d141727c68719b
Harry Young <hendrik.gerritzen@googlemail.com>
2012-11-11 00:16:35 +0100

    sync g_combat.c from rpgxef-repo
    Signed-off-by: Harry Young <hendrik.gerritzen@googlemail.com>

--- game/g_combat.c
+++ game/g_combat.c
@@ -1840,6 +1840,11 @@ void G_Repair(gentity_t *ent, gentity_t *tr_ent, float rate) {
 			max = tr_ent->r.maxs[i];
 		}
 	}
+
+	DEVELOPER(
+		G_Printf("G_Repair: target=%d, goodDist=%f, curDist=%f\n", tr_ent-g_entities, 80+max, distance);
+	);
+
 	if(distance > 80 + max) {
 		return;
 	}
